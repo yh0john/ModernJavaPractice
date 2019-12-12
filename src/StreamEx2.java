b466ab70df6047bf54eb9da4ea796f0f866ee217
Yohan Lee <yh0john@gmail.com>
2019-12-13 01:24:06 +0900

    Stream map 실습

--- src/StreamEx2.java
+++ src/StreamEx2.java
@@ -1,2 +1,28 @@
+import java.io.File;
+import java.util.stream.Stream;
+
 public class StreamEx2 {
+    public static void main(String args[]){
+        File[] fileArr ={new File("Ex1.java"),
+                new File("Ex1.bak"),
+                new File("Ex2.java"),
+                new File("Ex1"),
+                new File("Ex1.txt"),};
+
+        Stream<File> fileStream = Stream.of(fileArr);
+        //map()으로 Stream<File>을 Stream<String>으로 변환
+        Stream<String> filenameStream = fileStream.map(f->f.getName());
+        filenameStream.forEach(System.out::println);
+
+        //fileStream 재생성
+        fileStream = Stream.of(fileArr);
+
+        fileStream.map(f->f.getName())
+                .filter(s->s.indexOf('.')!=-1) //확장자가 없는 것 제외
+                .map(s->s.substring(s.indexOf('.')+1))  //확장자만 추출
+                .map(String::toUpperCase)   //모두 대문자로 변환
+                .distinct() //중복제거
+                .forEach(System.out::print); //출력
+
+    }
 }
