c6b76999ff2fcd45467f486bf227d29a4a6f4810
Yohan Lee <yh0john@gmail.com>
2019-12-19 23:32:04 +0900

    flatMap 실습

--- src/StreamEx4.java
+++ src/StreamEx4.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StreamEx4 {
+    public static void main(String[] args) {
+        Stream<String[]> strArrstrm = Stream.of(new String[]{"abc","def","ghi"},new String[]{"ABC","DEF","HIG"});
+
+        //이렇게 사용 할 경우 스트림의 스트림이 생성됨
+        //Stream<Stream<String>> strStrmStrm = strArrstrm.map(Arrays::stream);
+        Stream<String> strStrm = strArrstrm.flatMap(Arrays::stream);
+
+        strStrm.map(String::toLowerCase)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+        
+    }
 }
